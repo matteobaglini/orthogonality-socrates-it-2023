05aa08ff8d690aca568830b1a96ff9455db29051
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-28 09:23:10 +0200

    refactor(towards OCP): move check* functions

--- src/main/scala/Checks.scala
+++ src/main/scala/Checks.scala
@@ -1,3 +1,15 @@
 object Checks {
   type Check = String => Boolean
+
+  def checkLength: Check = {
+    password => password.length > 8
+  }
+
+  def checkUpperCase: Check = {
+    password => password.matches("^.*[A-Z].*$")
+  }
+
+  def checkNumber: Check = {
+    password => password.matches("^.*[0-9].*$")
+  }
 }
--- src/main/scala/PasswordValidator.scala
+++ src/main/scala/PasswordValidator.scala
@@ -1,4 +1,4 @@
-import Checks.Check
+import Checks._
 
 case class PasswordValidator() {
   def validate(password: String): Boolean = {
@@ -10,17 +10,5 @@ case class PasswordValidator() {
     checks
       .map(check => check(password))
       .reduce((acc, cur) => acc && cur)
-  }
-
-  private def checkLength: Check = {
-    password => password.length > 8
-  }
-
-  private def checkUpperCase: Check = {
-    password => password.matches("^.*[A-Z].*$")
-  }
-
-  private def checkNumber: Check = {
-    password => password.matches("^.*[0-9].*$")
   }
 }
