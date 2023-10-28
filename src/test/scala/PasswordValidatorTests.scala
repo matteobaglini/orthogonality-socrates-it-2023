a9a3dd14513701ce8084833cf06a0888acab2319
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-28 09:33:53 +0200

    test: test the PasswordValidator responsibility

--- src/test/scala/PasswordValidatorTests.scala
+++ src/test/scala/PasswordValidatorTests.scala
@@ -2,6 +2,22 @@ import Checks._
 
 class PasswordValidatorTests extends munit.FunSuite {
 
+  def validCheck:Check = _ => true
+  def invalidCheck:Check = _ => false
+
+  test("all checks are valid") {
+    val result = PasswordValidator(List(validCheck, validCheck, validCheck))
+      .validate("anything")
+    assert(result)
+  }
+
+  test("at least one check invalid") {
+    val result = PasswordValidator(List(validCheck, validCheck, invalidCheck))
+      .validate("anything")
+    assert(!result)
+  }
+
+
   val checks = List(
     checkLength,
     checkUpperCase,
