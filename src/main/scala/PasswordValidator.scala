f093982f46564caa60d58afc69d52f263846ec99
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-28 09:20:09 +0200

    refactor(towards OCP): inline local vars

--- src/main/scala/PasswordValidator.scala
+++ src/main/scala/PasswordValidator.scala
@@ -2,11 +2,10 @@ import Checks.Check
 
 case class PasswordValidator() {
   def validate(password: String): Boolean = {
-    val check1: Check = checkLength
-    val check2: Check = checkUpperCase
-    val check3: Check = checkNumber
-
-    val checks = List(check1, check2, check3)
+    val checks = List[Check](
+      checkLength,
+      checkUpperCase,
+      checkNumber)
 
     checks
       .map(check => check(password))
