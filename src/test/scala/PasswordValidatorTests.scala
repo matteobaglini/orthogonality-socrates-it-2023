31df76cbba7beef6321d1193db3e1056f9cd0a89
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-28 09:24:32 +0200

    refactor(towards OCP): extract field in test suite

--- src/test/scala/PasswordValidatorTests.scala
+++ src/test/scala/PasswordValidatorTests.scala
@@ -1,38 +1,40 @@
 
 class PasswordValidatorTests extends munit.FunSuite {
 
+  val passwordValidator = PasswordValidator()
+
   test("password with more than 8 chars") {
-    val result = PasswordValidator()
+    val result = passwordValidator
       .validate("1asdasdasD")
     assert(result)
   }
 
   test("password with less than 9 chars") {
-    val result = PasswordValidator()
+    val result = passwordValidator
       .validate("asdasd")
     assert(!result)
   }
 
   test("password with upper case letter") {
-    val result = PasswordValidator()
+    val result = passwordValidator
       .validate("asd7aSdasd")
     assert(result)
   }
 
   test("password missing upper case letter") {
-    val result = PasswordValidator()
+    val result = passwordValidator
       .validate("asdasdasd")
     assert(!result)
   }
 
   test("password with a number") {
-    val result = PasswordValidator()
+    val result = passwordValidator
       .validate("asdaSdasd9")
     assert(result)
   }
 
   test("password missing a number") {
-    val result = PasswordValidator()
+    val result = passwordValidator
       .validate("asdaSdasd")
     assert(!result)
   }
