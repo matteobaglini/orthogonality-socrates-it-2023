7d1a6498cf10bdc0c6de5469b5e6075d5d51857e
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-28 09:50:12 +0200

    refactor(towards Either): migrate PasswordValidator to parallel desing

--- src/main/scala/PasswordValidator.scala
+++ src/main/scala/PasswordValidator.scala
@@ -1,9 +1,9 @@
 import Checks._
 
-case class PasswordValidator(checks: List[Check]) {
-  def validate(password: String): Boolean = {
+case class PasswordValidator(checks: List[CheckE]) {
+  def validate(password: String): Either[ErrorMsg, Password] = {
     checks
       .map(check => check(password))
-      .reduce((acc, cur) => acc && cur)
+      .reduce((acc, cur) => acc.flatMap(_ => cur))
   }
 }
--- src/test/scala/PasswordValidatorTests.scala
+++ src/test/scala/PasswordValidatorTests.scala
@@ -2,18 +2,20 @@ import Checks._
 
 class PasswordValidatorTests extends munit.FunSuite {
 
-  def validCheck: Check = _ => true
-  def invalidCheck: Check = _ => false
+  def validCheck: CheckE = password => Right(password)
+  def invalidCheck: CheckE = _ => Left("invalid")
 
   test("all checks are valid") {
     val result = PasswordValidator(List(validCheck, validCheck, validCheck))
       .validate("anything")
-    assert(result)
+    val expected = Right("anything")
+    assertEquals(result, expected)
   }
 
   test("at least one check invalid") {
     val result = PasswordValidator(List(validCheck, validCheck, invalidCheck))
       .validate("anything")
-    assert(!result)
+    val expected = Left("invalid")
+    assertEquals(result, expected)
   }
 }
